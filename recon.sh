2160ddf89814aacf1da687bce2b6409562fe6505
SK Rahimul Haque <118619230+fa1consec@users.noreply.github.com>
2022-11-21 23:32:08 +0530

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -15,3 +15,7 @@ findomain -t $domain -q --threads 50 -u $domain.txt
 echo "Remove Duplicates"
 cat amass_$domain.txt subfinder_$domain.txt findomain_$domain.txt > domains.txt | sort -u | tee domains.txt
 cat domains.txt |  httpx-toolkit -r 1.1.1.1 -o alive_domains.txt -silent
+
+echo "Subdomain Takeover"
+sed 's/https\?:\/\///' alive_domains.txt | tee takeover_testing.txt
+subjack -c fingerprints.json -w takeover_testing.txt -t 50
