16a4ef7e87f8f960a2bf0af9835822dbfd1e36e1
SK Rahimul Haque <118619230+fa1consec@users.noreply.github.com>
2022-11-21 23:22:35 +0530

    Add files via upload

---
+++ recon.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+echo "Welcome to Recon"
+read -p "Enter a Domain: " domain
+
+echo "Start Amass"
+amass enum --passive -silent -tr 1.1.1.1 -o amass_$domain.txt -d $domain
+
+echo "Start Subfinder"
+subfinder -d $domain -r 1.1.1.1 -silent -t 50 -o subfinder_$domain.txt
+
+echo "Start Findomain"
+findomain -t $domain -q --threads 50 -u $domain.txt 
+
+echo " Remove Duplicates"
+cat amass_$domain.txt subfinder_$domain.txt findomain_$domain.txt > domains.txt | sort -u | tee domains.txt
+cat domains.txt |  httpx-toolkit -r 1.1.1.1 -o alive_domains.txt -silent
